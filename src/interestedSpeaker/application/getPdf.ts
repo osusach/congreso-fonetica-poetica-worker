e5b328576a20c896923e54033aa9818f09849c58
kappita <ignacioladal@gmail.com>
2024-07-03 19:58:14 -0300

    chore: remove interested speaker as it is no longer used

--- src/interestedSpeaker/application/getPdf.ts
+++
@@ -1,9 +0,0 @@
-export const getFile = async (id: string, env: Bindings) => {
-	const object = await env.PDF_BUCKET.get(id);
-
-	if (object === null) {
-		return { success: false, error: 'Pdf not found' };
-	}
-
-	return { body: object.body };
-};
--- src/interestedSpeaker/application/interestedSpeakerEmailTemplate.ts
+++
@@ -1,4 +0,0 @@
-
-export const correoInterested = (name: string) =>`
-
-`
